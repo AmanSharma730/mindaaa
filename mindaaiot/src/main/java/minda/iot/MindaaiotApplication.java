228a65a0c53b207c0aa0cd741c42d86e01caa26a
as560 <as560@DESKTOP-MUJ9OD2>
2023-10-30 15:10:14 +0530

    pushing data

---
+++ mindaaiot/src/main/java/minda/iot/MindaaiotApplication.java
@@ -0,0 +1,13 @@
+package minda.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindaaiotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindaaiotApplication.class, args);
+	}
+
+}
---
+++ mindaaiot/src/test/java/minda/iot/MindaaiotApplicationTests.java
@@ -0,0 +1,13 @@
+package minda.iot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindaaiotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
